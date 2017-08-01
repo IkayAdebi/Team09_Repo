131ef5b9dffd309e6880d0885537cfe8ecbb9096
LAPTOP-M7VGAAAU\Adebi Family <iadebi@andrew.cmu.edu>
2017-08-01 14:47:08 -0400

    Edited some values

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,8 +11,8 @@ public class Player : MonoBehaviour {
     public const string VERTICAL_AXIS = "Vertical";
 
     private float _moveSpeed = .1f;
-    private float _jumpStrength = 11f; // old value: 350
-    private const float MAX_FALL_SPEED = -15;
+    private float _jumpStrength = 16f; // old value: 350
+    private const float MAX_FALL_SPEED = -20f;
     
     public static bool isGrounded;
     private bool isJumping;
@@ -33,7 +33,7 @@ public class Player : MonoBehaviour {
     void Start () {
         _rb = gameObject.GetComponent<Rigidbody2D>();
         isGrounded = true;
-        _rb.gravityScale *= 2;
+        _rb.gravityScale *= 2.5f;
         anim = GetComponent<Animator>();
 	}
 
@@ -49,11 +49,11 @@ public class Player : MonoBehaviour {
                 // Flip Character based off of movement
                 if (Input.GetAxis(HORIZONTAL_AXIS) > 0)
                 {
-                    transform.localScale = new Vector3(.6f, .6f, 1);
+                    transform.localScale = new Vector3(1, 1, 1);
                 }
                 else
                 {
-                    transform.localScale = new Vector3(-.6f, .6f, 1);
+                    transform.localScale = new Vector3(-1, 1, 1);
                 }
             }
             else
@@ -65,11 +65,11 @@ public class Player : MonoBehaviour {
             transform.position = new Vector3(transform.position.x + Input.GetAxis(HORIZONTAL_AXIS) * _moveSpeed, transform.position.y, 0);
 
             // Sets Jumping flags to true based off of player 1's input
-            if (Input.GetButtonDown("Jump") && isGrounded) //&& !(Input.GetAxis(VERTICAL_AXIS) < 0))
+            if (Input.GetButtonDown("Jump") && isGrounded)
             {
                 isJumping = true;
             }
-            if ((Input.GetButtonUp("Jump") || Input.GetAxis(VERTICAL_AXIS) < 0) && !isGrounded)
+            if ((Input.GetButtonUp("Jump") || Input.GetAxis(VERTICAL_AXIS) < -0.5f) && !isGrounded)
             {
                 isJumpCanceled = true;
             }
