85e7e0a60b7d44116c8a50ee7e32c04a9be14a5c
Benjamin Cagan <bcagan@andrew.cmu.edu>
2017-08-02 09:59:54 -0400

    Changes including new snow effect as of 8/2/17 at 9:59 AM

---
+++ Assets/Scripts/SnowEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowEffect : MonoBehaviour {
+
+    public Player playerOne;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("I need to blow my nose which could also be a wind pun hahaha");
+        
+        if (other.gameObject.tag == "Player")
+        {
+            playerOne.
+        }
+
+    }
+}
