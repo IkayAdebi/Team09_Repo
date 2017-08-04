ce6a2bc06c5f2284e5a8039b05b866c51f2c9ee6
Benjamin Cagan <bcagan@andrew.cmu.edu>
2017-08-04 15:33:46 -0400

    Hey can you please ******* commit

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    public GameObject player;
+    private Player script;
+	// Use this for initialization
+	void Start () {
+        script = player.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            script.currentCheckpoint = gameObject;
+        }
+
+    }
+}
