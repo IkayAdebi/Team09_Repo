5ee521b862a65284721cc42590eca26e7bf083a3
LAPTOP-M7VGAAAU\Adebi Family <iadebi@andrew.cmu.edu>
2017-08-07 09:47:57 -0400

    no message

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
     public GameObject player;
+<<<<<<< HEAD
+=======
     public GameObject wp;
+>>>>>>> 8548c9ead7acb528636a05b50e85b1297eea9c7b
     private Player script;
 	// Use this for initialization
 	void Start () {
         script = player.GetComponent<Player>();
+<<<<<<< HEAD
+	}
+=======
         wp = GameObject.Find("Player 2");
     }
+>>>>>>> 8548c9ead7acb528636a05b50e85b1297eea9c7b
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,11 +30,15 @@ public class Checkpoint : MonoBehaviour {
         {
             script.currentCheckpoint = gameObject;
         }
+<<<<<<< HEAD
+
+=======
         wp.GetComponent<WeatherPlayer>().inCheck = true;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         wp.GetComponent<WeatherPlayer>().inCheck = false;
+>>>>>>> 8548c9ead7acb528636a05b50e85b1297eea9c7b
     }
 }
