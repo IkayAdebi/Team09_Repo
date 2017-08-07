9525d8d67aa115f504ad133c8ec4750a0e47baca
Jacob Asofsky <jasofsky@andrew.cmu.edugit commit -m helloworldhellogit commit -m helloworldgit config --global user.email jasofsky@andrew.cmu.edu>
2017-08-07 09:33:23 -0400

    no message

---
+++ Assets/Indicate.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicate : MonoBehaviour {
+	private GameObject p2;
+	public Sprite off;
+	public Sprite on;
+	public int index;
+
+	private float Counter;
+	private float Cooldown;
+
+	// Use this for initialization
+	void Start () {
+		p2 = GameObject.Find("Player 2");
+		//Cooldown = p2.GetComponent<WeatherPlayer> ().windCooldown;
+	
+		if (index == 0) {
+			Cooldown = p2.GetComponent<WeatherPlayer> ().windCooldown;
+		} 
+		else if (index == 1) {
+			Cooldown = p2.GetComponent<WeatherPlayer> ().snowCooldown;
+		}
+		else if (index == 2) {
+			Cooldown = p2.GetComponent<WeatherPlayer> ().lightningCooldown;
+		}
+		else if (index == 3) {
+			Cooldown = p2.GetComponent<WeatherPlayer> ().earthquakeCooldown;
+		}
+		else if (index == 4) {
+			Cooldown = p2.GetComponent<WeatherPlayer> ().fireCooldown;
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Counter = p2.GetComponent<WeatherPlayer> ().windCounter;
+
+
+		if (index == 0) {
+			Counter = p2.GetComponent<WeatherPlayer> ().windCounter;
+		} 
+		else if (index == 1) {
+			Counter = p2.GetComponent<WeatherPlayer> ().snowCounter;	
+		}
+		else if (index == 2) {
+			Counter = p2.GetComponent<WeatherPlayer> ().lightningCounter;	
+		}
+		else if (index == 3) {
+			Counter = p2.GetComponent<WeatherPlayer> ().earthquakeCounter;
+		}
+		else if (index == 4) {
+			Counter = p2.GetComponent<WeatherPlayer> ().fireCounter;
+		}
+
+
+
+
+		if(Counter >= Cooldown) {
+			gameObject.GetComponent<SpriteRenderer> ().sprite = on;
+		}
+		else gameObject.GetComponent<SpriteRenderer> ().sprite = off;
+
+
+
+
+	
+
+
+
+	}
+}
--- Assets/Scripts/LightningStrike.cs
+++ Assets/Scripts/LightningStrike.cs
@@ -6,18 +6,18 @@ public class LightningStrike : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine("Fizzle");
+//		StartCoroutine("Fizzle");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-	IEnumerator Fizzle()
-	{
-		yield return new WaitForSeconds (0.1F);
 		Destroy (gameObject);
-
 	}
+
+	/** IEnumerator Fizzle()
+	{
+	//	yield return new WaitForSeconds (0.01F);
+		//Destroy (gameObject);
+
+	//} **/
 }
